e84a9ada1c921dff7f7ab627adfe9eddeb353e67
Mateo Adzaga <mateo_a1@hotmail.com>
2021-01-10 15:52:48 +0100

    added routing

---
+++ src/Jobfinder.tsx
@@ -0,0 +1,16 @@
+const Jobfinder = () => {
+
+    return (
+      <>
+      <div style={{backgroundColor: '#303F9F', flex: 1}}>
+
+      </div>
+      <div style={{flex: 1}}>
+
+      </div>
+      </>
+    );
+  }
+  
+  export default Jobfinder;
+  
