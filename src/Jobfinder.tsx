30d13ed49c50d46cc69662ae193ba92a24f748fc
Mateo Adzaga <mateo_a1@hotmail.com>
2021-01-10 16:11:51 +0100

    yolo

--- src/Jobfinder.tsx
+++ src/Jobfinder.tsx
@@ -1,16 +1,16 @@
 const Jobfinder = () => {
-
     return (
-      <>
-      <div style={{backgroundColor: '#303F9F', flex: 1}}>
-      <h2>Test</h2>
-      </div>
-      <div style={{flex: 1}}>
-
-      </div>
-      </>
+        <>
+        <div style={{  display:'flex', flexDirection: 'column'}}>
+            <div style={{ backgroundColor: '#303F9F', flex: 1 }}>
+                <h2>Test</h2>
+            </div>
+            <div style={{ flex: 1 }}>
+                <h2>Test</h2>
+            </div>
+        </div>
+        </>
     );
-  }
-  
-  export default Jobfinder;
-  
+}
+
+export default Jobfinder;
