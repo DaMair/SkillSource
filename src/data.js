bbcf48b00ec8e018675f2fb7bc1b3bf8c2ed9b54
Mateo Adzaga <mateo_a1@hotmail.com>
2021-01-11 23:06:28 +0100

    another one

--- src/data.js
+++ src/data.js
@@ -22,4 +22,19 @@ export const jobs = [{
     title: 'Test',
     description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
     skills: 'Test Angular Yolo Fuck',
+},
+{
+    title: 'Test',
+    description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+    skills: 'Test Angular Yolo Fuck',
+},
+{
+    title: 'Test',
+    description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+    skills: 'Test Angular Yolo Fuck',
+},
+{
+    title: 'Test',
+    description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+    skills: 'Test Angular Yolo Fuck',
 }];
